ee9218724f80d125a1e7ece21562a1c81d52acea
Florian Odronitz <odo@mac.com>
2026-05-27 00:40:05 +0200

    reformatting

--- lib/drizzle/time.ex
+++ lib/drizzle/time.ex
@@ -1,7 +1,8 @@
 defmodule Drizzle.Time do
 
   def now() do
-    DateTime.utc_now() |> to_seconds() 
+    DateTime.utc_now()
+    |> to_seconds() 
   end
 
   def from_seconds(seconds, time_zone) when is_integer(seconds) and is_binary(time_zone) do
@@ -12,6 +13,7 @@ defmodule Drizzle.Time do
   end
 
   def to_seconds(%DateTime{} = time) do
-    time |> DateTime.to_gregorian_seconds() |> elem(0)
+    time
+    |> DateTime.to_gregorian_seconds() |> elem(0)
   end
 end
